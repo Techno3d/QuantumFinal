b849bcaccb5e12137d97c7a983f38f28d2c8f62c
Shadman Syed <shadman3d@gmail.com>
2025-06-05 03:12:15 -0400

    some more fixes?

--- Einstein/qftPW.gd
+++ Einstein/qftPW.gd
@@ -4,10 +4,10 @@ extends Node3D
 @onready var text_PW: MeshInstance3D = $TextBackgroundPW
 
 func _ready():
-    text_PW.hide()
     area.body_entered.connect(func(x):
         text_PW.show()
     )
     area.body_exited.connect(func(x):
         text_PW.hide()
     )
+    text_PW.hide()
