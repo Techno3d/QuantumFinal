cc0b83ab39ee2e4461e2fb55d1d744d320b28b3b
Shadman Syed <shadman3d@gmail.com>
2025-06-04 20:18:30 -0400

    pilot wave beginnings

--- DoubleSlit/Scripts/plane.gd
+++ DoubleSlit/Scripts/plane.gd
@@ -1,17 +1,9 @@
 extends MeshInstance3D
 
 @onready var particle: MeshInstance3D = $"../Particle"
-var stored_poses: Array[Vector3];
-
-func _ready():
-	for i in range(0, 15):
-		stored_poses.push_back(Vector3(0, 0.4/15.0*i, 0));
+var stored_pos: Vector3;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	for i in range(0, stored_poses.size()):
-		stored_poses[i].y += delta
-		if stored_poses[i].y > 0.4:
-			stored_poses[i] = particle.global_position
-			stored_poses[i].y = 0
-	get_surface_override_material(0).set_shader_parameter("pos", stored_poses)
+func _process(_delta):
+    stored_pos = particle.global_position
+    get_surface_override_material(0).set_shader_parameter("pos", stored_pos)
---
+++ DoubleSlit/copenhagen_double_slit.gd
@@ -0,0 +1,28 @@
+extends Node3D
+
+var point1: Vector2 = Vector2(1.348, 0)
+var point2: Vector2 = Vector2(-1.348, 0)
+
+@onready var timer: Timer = $ParticleMeasureTimer
+var particle: PackedScene = preload("res://DoubleSlit/particle.tscn")
+
+func _ready():
+	timer.timeout.connect(spawn_particle)
+
+func spawn_particle():
+	var p_amp: float = find_p_amp(0);
+	p_amp *= p_amp
+	for i in range(-1000, 1000, 50):
+		var j = i/100.
+		# This is the trapezoidal reimann aproximation for the probability
+		var probs = (find_p_amp(j)*find_p_amp(j) + find_p_amp(j+0.05)*find_p_amp(j+0.05))
+		if randf() > probs and randf() > 0.8:
+			var child: MeshInstance3D = particle.instantiate();
+			add_child(child)
+			child.position = Vector3(j+0.025, 0.5+randf_range(-0.4, 3), 10.2)
+			# break
+
+
+
+func find_p_amp(x: float) -> float:
+	return (sin(16*PI*point1.distance_to(Vector2(x, 10.6))) + sin(16*PI*point2.distance_to(Vector2(x, 10.6))))/2.
