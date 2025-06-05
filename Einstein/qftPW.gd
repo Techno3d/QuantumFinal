f555e425b2d9d77cea3e2e315295710bfe0ee1cb
Shadman Syed <shadman3d@gmail.com>
2025-06-05 02:09:52 -0400

    yeayeayeeayeayea wow

---
+++ Einstein/qftPW.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+@onready var area: Area3D = $Area3D
+@onready var text_PW: MeshInstance3D = $TextBackgroundPW
+
+func _ready():
+    text_PW.hide()
+    area.body_entered.connect(func():
+        text_PW.show()
+    )
+    area.body_exited.connect(func():
+        text_PW.hide()
+    )
